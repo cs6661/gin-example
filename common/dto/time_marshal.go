49279cd60527cbfc84f9a79042461c784698a2ad
caosi <caosi@25qp.cn>
2024-11-12 20:23:20 +0800

    自定义时间

--- common/dto/time_marshal.go
+++ common/dto/time_marshal.go
@@ -8,9 +8,21 @@ import (
 
 type LocalTime time.Time
 
+// 1.在使用 gorm 框架时，用到 MarshalJSON / Scan / Value
+// 2.在使用 Gin 框架时，使用 ShouldBindJSON 绑定参数会调用 UnmarshalJSON，context.JSON 返回 json 时会调用 MarshalJSON
+
 func (t *LocalTime) MarshalJSON() ([]byte, error) {
 	tTime := time.Time(*t)
 	return []byte(fmt.Sprintf("\"%v\"", tTime.Format(time.DateTime))), nil
+}
+
+func (t *LocalTime) UnmarshalJSON(data []byte) error {
+	if string(data) == `""` {
+		return nil
+	}
+	parseInLocation, err := time.ParseInLocation(`"`+time.DateTime+`"`, string(data), time.Local)
+	*t = LocalTime(parseInLocation)
+	return err
 }
 
 func (t LocalTime) Value() (driver.Value, error) {
